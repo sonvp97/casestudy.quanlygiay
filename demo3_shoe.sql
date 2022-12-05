bb16a63e6ba25a7ba774da5b5077ab60823a9cc0
sonvp97 <112803870+sonvp97@users.noreply.github.com>
2022-12-05 10:49:42 +0700

    databa

---
+++ demo3_shoe.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: demo3
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `shoe`
+--
+
+DROP TABLE IF EXISTS `shoe`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shoe` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `describe` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `brand` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `size` int DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `img` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shoe`
+--
+
+LOCK TABLES `shoe` WRITE;
+/*!40000 ALTER TABLE `shoe` DISABLE KEYS */;
+INSERT INTO `shoe` VALUES (1,'Adidas','Milk',233333,'nice',12,12,'giay1.jpg'),(13,'Adidas','123',123,'123',123,123,'giay2.jpg'),(14,'NIKE COURT ROYALE','Milk',32434,'nice',43,44,'giay3.jpg'),(15,'CHRISTIAN LOUBOUTIN','GIÃY LÆ¯á»I CHRISTIAN LOUBOUTIN LOAFER SPOOKY FLAT CALF P/JACQ MÃU ÄEN XÃM',36000000,'Christian Louboutin',36,40,'giay4.jpg'),(16,'GIÃÂÃÂY SLIP ON','GIÃÂÃÂY SLIP ON BURBERRY MONOGRAM PRINT COTTON CANVAS ESPADRILLES MÃÂÃÂU BE/ÃÂÃÂEN',2600000,'Burberry',36,45,'dep1.jpg'),(17,'asd','asd',1231,'nice',45,44,'dep1.jpg');
+/*!40000 ALTER TABLE `shoe` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-05 10:49:01
